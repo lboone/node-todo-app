8af98d0f3dbf64ac59cfa7d5087f010a6c8c39fd
Lloyd Boone <lloyd@lambapps.com>
2018-07-23 13:25:38 -0400

    Add PATCH /todos/:id route and included lodash library

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -59,6 +60,29 @@ app.delete('/todos/:id',(req,res)=>{
         }).catch((error) => res.status(400).send({'Error':error}));
     } else {
         res.status(404).send({'Error':'ID not valid!'});
+    }
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(ObjectID.isValid(id)){
+        var body = _.pick(req.body, ['text','completed']);
+        
+        if(_.isBoolean(body.completed) && body.completed){
+            body.completedAt = new Date().getTime();
+        } else {
+            body.completed = false;
+            body.completedAt = null;
+        }
+
+        Todo.findByIdAndUpdate(id,{$set:body},{new: true}).then((todo) => {
+            if(!todo){
+                return res.status(404).send({'Error':'Todo does not exist'});
+            }
+            res.send((todo));
+        }).catch((e) => res.status(400).send());
+    } else {
+        return res.status(404).send({'Error':'ID not valid!'});
     }
 });
 
