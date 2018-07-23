7b4e02b99f5899cf10e12b39d7aecedee861f1d7
Lloyd Boone <lloyd@lambapps.com>
2018-07-23 11:38:09 -0400

    Setup app for heroku - repair

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 // Setup mongoose to use the default Promise
 mongoose.Promise = global.Promise;
 // Connect mongoose to my mongodb server
-mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true } );
+var server = process.env.MONGODB_URL || 'mongodb://localhost:27017/BusinessQueues';
+mongoose.connect(server,{ useNewUrlParser: true } );
 
 module.exports.mongoose = mongoose;
