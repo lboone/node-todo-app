5cdd8a4f489b81457a684d79a68c631c543e12e4
Lloyd Boone <lloyd@lambapps.com>
2018-07-25 20:53:15 -0400

    Fix config file

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config.json');
 
 if (env === 'development' || env === 'test'){
+    const config = require('./config.json');
     var envConfig = config[env];
     Object.keys(envConfig).forEach((key)=> {
         process.env[key] = envConfig[key];
